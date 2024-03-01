1a6b4615e48e27c487af5d2cb884488974d8eeb3
no80dy <no8odyas@gmail.com>
2024-03-01 13:53:30 +0300

    Добавлен функционал синхронной перемотки и продолжения после паузы

--- backend/party-manager-service/src/services/websocket.py
+++ backend/party-manager-service/src/services/websocket.py
@@ -26,11 +26,12 @@ class WebSocketConnectionService:
         await websocket.accept()
         self.websocket_router.add_connection(party_id, websocket)
         for websocket_connection in self.websocket_router.get_websocket_by_party_id(party_id):
-            await websocket_connection.send_text(
-                json.dumps(
-                    {"type": "chat", "text": f"{username} joined to the party!"}
+            if websocket_connection != websocket:
+                await websocket_connection.send_text(
+                    json.dumps(
+                        {"type": "chat", "text": f"{username} joined to the party!"}
+                    )
                 )
-            )
         try:
             while True:
                 websocket_connections = \
@@ -39,23 +40,46 @@ class WebSocketConnectionService:
                 message_json = json.loads(message)
                 if message_json["type"] == "chat":
                     for websocket_connection in websocket_connections:
-                        await websocket_connection.send_text(
-                            json.dumps(
-                                {
-                                    "type": "chat",
-                                    "text": f"{username} says {json.loads(message)['text']}"
-                                }
+                        if websocket_connection != websocket:
+                            await websocket_connection.send_text(
+                                json.dumps(
+                                    {
+                                        "type": "chat",
+                                        "text": f"{username} says {json.loads(message)['text']}"
+                                    }
+                                )
                             )
-                        )
                 elif message_json["type"] == "pause":
                     for websocket_connection in websocket_connections:
-                        await websocket_connection.send_text(
-                            json.dumps(
-                                {
-                                    "type": "pause",
-                                }
+                        if websocket_connection != websocket:
+                            await websocket_connection.send_text(
+                                json.dumps(
+                                    {
+                                        "type": "pause",
+                                    }
+                                )
                             )
-                        )
+                elif message_json["type"] == "play":
+                    for websocket_connection in websocket_connections:
+                        if websocket_connection != websocket:
+                            await websocket_connection.send_text(
+                                json.dumps(
+                                    {
+                                        "type": "play",
+                                    }
+                                )
+                            )
+                elif message_json["type"] == "seeked":
+                    for websocket_connection in websocket_connections:
+                        if websocket_connection != websocket:
+                            await websocket_connection.send_text(
+                                json.dumps(
+                                    {
+                                        "type": "seeked",
+                                        "time": message_json["time"]
+                                    }
+                                )
+                            )
                 await asyncio.sleep(1)
         except WebSocketDisconnect:
             self.websocket_router.remove_connection(party_id, websocket)
