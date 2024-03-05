78bf60850d9fc52562a8162dfa41b907e7ed1e23
sergei <si32cs50@gmail.com>
2024-03-05 20:33:55 +0300

    Auth заменили на Mock-auth

---
+++ mock-auth-service/src/services/user.py
@@ -0,0 +1,24 @@
+import json
+import logging
+
+from functools import lru_cache
+
+from db.users import users_db
+from models.models import User
+
+
+class UserService:
+    @staticmethod
+    async def check_user_exist(
+            username: str,
+            password: str,
+    ) -> User | None:
+        if username in users_db:
+            if password == users_db[username]['password']:
+                return User(**users_db[username])
+        return None
+
+
+@lru_cache()
+def get_user_service() -> UserService:
+    return UserService()
